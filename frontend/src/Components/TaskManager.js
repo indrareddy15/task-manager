7e75213410e4cfcd75b432f14c13140a8c290274
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-17 21:28:49 +0530

    task1

---
+++ frontend/src/Components/TaskManager.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Box, TableContainer, Table, TableHead, TableBody, TableRow, TableCell } from "@mui/material";
+
+
+const TaskManagerLite = () => {
+
+
+    return (
+        <Box>
+            <TableContainer>
+                <Table>
+                    
+                </Table>
+            </TableContainer>
+        </Box>
+    )
+}
+
