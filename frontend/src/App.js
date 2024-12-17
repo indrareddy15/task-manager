0dc17a0977aa847fa0f93a65aa2e3354f70861aa
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-17 21:29:26 +0530

    appjs

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TaskManager from "./Components/task";
+
+function App() {
+  return (
+    <div className="container">
+      <TaskManager />
+    </div>
+  );
+}
+
+export default App;
