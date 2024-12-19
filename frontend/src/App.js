43e57acdea995bcb2330b66061ae9239c5e8d63f
indrareddy15 <gantaindrareddy83@gmail.com>
2024-12-19 23:00:42 +0530

    AppJs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { Container } from "@mui/material";
 import { TaskManager } from "./components/TaskManager";
 import { Nav } from "./components/AppBar";
+import TaskManager2 from "./compo/prime";
 
 const App = () => {
   return (
     <>
       <Nav />
       <Container maxWidth="lg">
-        <TaskManager />
+        {/* <TaskManager /> */}
+        <TaskManager2 />
       </Container>
     </>
   );
