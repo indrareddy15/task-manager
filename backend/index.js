e5e69fd2fe53996fea289f1a592033e46fea0bba
indrareddy15 <gantaindrareddy83@gmail.com>
2025-01-02 22:50:41 +0530

    server

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./db/db');
 const taskRoutes = require('./routes/task.routes');
 
 dotenv.config();
 const app = express();
 
+const corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    credentials: true
+}
+app.use(cors(corsOptions))
 const port = process.env.PORT || 3000;
 
 connectDB();
 
 app.use(express.json());
 
-// app.use(express.json());
-app.use("/tasks/v1", taskRoutes)
+app.use("/tasks", taskRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
