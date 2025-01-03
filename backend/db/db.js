3bc372ad822428f65528c3dc007c78f7a65918ac
Ganta Indra Reddy <47188187+indrareddy15@users.noreply.github.com>
2025-01-03 16:55:16 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,23 +5,21 @@ require('dotenv').config();
 const mongoURI = process.env.MONGO_URI;
 
 if (!mongoURI) {
-  console.error("MongoDB URI is not defined in environment variables.");
-  process.exit(1); 
+    console.error("MongoDB URI is not defined in environment variables.");
+    process.exit(1);
 }
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true, 
-      useFindAndModify: false, 
-    });
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1); 
-  }
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
