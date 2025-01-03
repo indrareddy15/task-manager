6cbe35c3b0b5181f9fd6d3a101524a17074a5b22
Ganta Indra Reddy <47188187+indrareddy15@users.noreply.github.com>
2025-01-03 16:20:12 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('MongoDB is connected');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
